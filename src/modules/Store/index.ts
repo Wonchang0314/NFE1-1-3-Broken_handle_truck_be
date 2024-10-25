fd44251bcb0070584265c5e1d8f525bd36b24a29
Kim-HyounSuk <kkoor586@naver.com>
2024-10-25 17:06:43 +0900

    Refactor: 인증 미들웨어 배치

--- src/modules/Store/index.ts
+++ src/modules/Store/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getStoresController, postStoreController } from './controller';
+import { authHandler } from '@/middlewares';
 
 const storeRouter = Router();
 
 storeRouter.route('/').get(getStoresController);
-storeRouter.route('/').post(postStoreController);
+storeRouter.route('/').all(authHandler).post(postStoreController);
 
 export default storeRouter;
