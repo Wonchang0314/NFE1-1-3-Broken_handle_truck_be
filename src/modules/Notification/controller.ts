2dc601860f04ff8fc69f2a2f802f6eaa3e231337
Wonchang0314 <andrew0314@naver.com>
2024-11-01 14:42:08 +0900

    FIX: 알림 보내기 로직 수정

--- src/modules/Notification/controller.ts
+++ src/modules/Notification/controller.ts
@@ -14,15 +14,15 @@ export const postNotificationController = async (
 	next: NextFunction,
 ) => {
 	try {
-		const { storeId } = req.body;
-		if (!storeId)
+		const user = req.user;
+		if (!user?._id)
 			throw new AppError('알림 송신을 위한 가게ID가 누락되었습니다', 400);
 
-		const store = await Store.findOne({ _id: storeId });
+		const store = await Store.findOne({ ownerId: user?._id });
 
 		if (store === null) throw new AppError('존재하지 않는 가게입니다', 404);
 
-		const Notification = await postNotification(storeId);
+		const Notification = await postNotification(user?._id);
 		res.status(201).json({
 			msg: 'ok',
 			Notification,
--- src/modules/Notification/service.ts
+++ src/modules/Notification/service.ts
@@ -5,37 +5,35 @@ import { WebSocketWithUserId } from '../webSocketServer';
 import { Store } from '@/models';
 import Bookmark from '@/models/Bookmark';
 
-const getBookmarkedUserIds = async (
-	storeId: Types.ObjectId,
-): Promise<string[]> => {
-	const bookmark = await Bookmark.find({ storeId }).select('userId');
+const getBookmarkedUserIds = async (ownerId: string): Promise<string[]> => {
+	const bookmark = await Bookmark.find({ ownerId }).select('userId');
 
 	return bookmark.map((bookmark) => String(bookmark.userId));
 };
 
 export const postNotification = async (
-	storeId: Types.ObjectId,
+	ownerId: string,
 ): Promise<INotification> => {
 	const store = await Store.findOneAndUpdate(
-		{ _id: storeId },
+		{ _id: ownerId },
 		[{ $set: { isOpen: { $not: '$isOpen' } } }],
 		{ new: true },
 	);
 	// 해당 가게를 즐겨찾기한 사용자 ID 목록 조회
-	const bookmarkedUsers = await getBookmarkedUserIds(storeId);
+	const bookmarkedUsers = await getBookmarkedUserIds(ownerId);
 
 	let notification;
 	if (store!.isOpen) {
 		notification = new Notification({
 			recipients: bookmarkedUsers,
-			sender: storeId,
+			sender: ownerId,
 			type: 'open',
 			content: `${store?.category}가게가 영업을 시작했습니다`,
 		});
 	} else {
 		notification = new Notification({
 			recipients: bookmarkedUsers,
-			sender: storeId,
+			sender: ownerId,
 			type: 'closed',
 			content: `${store?.category}가게가 영업을 마감했습니다`,
 		});
